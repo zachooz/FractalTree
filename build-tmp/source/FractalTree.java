9e03afcfe73f166087a2f61ec9fd9e5c9c83bb59
zachooz <hoffking@gmail.com>
2015-01-14 10:35:20 -0800

    Tree Made

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,10 +1,28 @@
-import java.lang.*;
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.lang.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+
+private double fractionLength = .8f; 
 private int smallestBranch = 2; 
 private double branchAngle = 0;  
 private float theWeight = 1;
 private int theLength = 1;
-private float incrementer = 1.5;
+private float incrementer = 1.5f;
 public void setup(){   
 	size(600,600);    
 	frameRate(30); 
@@ -15,16 +33,25 @@ public void draw(){
 	drawBranches(width/2,height, theLength, -Math.PI/2, theWeight);
 	if(theLength<200){
 		theLength++;
-		theWeight+=.05;
-		branchAngle+=0.1;
+		theWeight+=.05f;
+		branchAngle+=0.1f;
 	}
 } 
 public void drawBranches(int x,int y, double branchLength, double angle, float weight) {  
 	strokeWeight(weight);
-	weight=(float)(weight/1.5);   
+	weight=(float)(weight/1.5f);   
 	if(branchLength > smallestBranch){
 		line(x, y, (float)(x + (branchLength*Math.cos(angle))), (float)(y + (branchLength*Math.sin(angle))));
 		drawBranches((int)(x + (branchLength*Math.cos(angle))),(int)(y + (branchLength*Math.sin(angle))), branchLength/incrementer, angle+branchAngle,weight);
 		drawBranches((int)(x + (branchLength*Math.cos(angle))),(int)(y + (branchLength*Math.sin(angle))), branchLength/incrementer, angle-branchAngle,weight); 
 	}
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
